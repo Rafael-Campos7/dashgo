3b64825035e21dabfff5a67497313ce3950e6483
Rafael Campos <rafaelscampos15@gmail.com>
2021-07-02 16:34:03 -0300

    Created SignIn page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,36 @@
-import Head from 'next/head'
+import React from 'react'
+import { Input } from '../components/Form/Input'
+import { Button, Flex, Stack } from '@chakra-ui/react'
 
-
-export default function Home() {
+export default function SignIn() {
   return (
-    <h1>dashgo</h1>
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        as="form"
+        direction="column"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+      >
+        <Stack spacing="4">
+          <Input name="email" label="E-mail" type="email" />
+          <Input name="password" label="Senha" type="password" />
+        </Stack>
+
+        <Button
+          type="submit"
+          size="lg"
+          mt="6"
+          colorScheme="pink"
+        >Entrar</Button>
+      </Flex>
+    </Flex>
   )
 }
